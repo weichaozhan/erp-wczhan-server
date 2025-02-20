d8714ca45f3de347b5edd9d447df88963ec405b0
weichaozhan <921391554@qq.com>
2025-02-20 21:37:47 +0800

    first API

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: '用户名不能为空' })
+  username: string;
+
+  @IsNotEmpty({ message: '密码不能为空' })
+  @Matches(
+    /^(?=.*\d)(?!.*(\d)\1{2})(?!.*(012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210))(?=.*[a-zA-Z])(?=.*[^\da-zA-Z\s]).{1,9}$/,
+    {
+      message:
+        '至少包含字母、数字、特殊字符，1-9位，并且不能连续出现3个大小连续或相同的数字(如：456、654、888)',
+    },
+  )
+  password: string;
+}
